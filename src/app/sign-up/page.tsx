f131bce23a7b7bc7467994e65c3429aba64202ae
Jacob <Jacobshortall@gmail.com>
2025-03-31 13:50:39 +0100

    Add sign up component

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,102 @@
+'use client';
+
+import {
+    Box,
+    Card,
+    Container,
+    Heading,
+    TextField,
+    Button,
+    Text,
+    Flex
+} from '@radix-ui/themes';
+import { useState } from 'react';
+
 const SignUp = () => {
-    return <h1>Sign Up</h1>;
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
+    return (
+        <Container className="standard-wrapper page-wrapper">
+            <Box className="form_container" maxWidth="540px">
+                <Card size="4">
+                    <Heading as="h1" className="form_heading" size="7">
+                        Sign up
+                    </Heading>
+
+                    <form>
+                        <Box className="form_input-container">
+                            <Flex
+                                justify="between"
+                                className="form_label-container"
+                            >
+                                <Text
+                                    as="label"
+                                    htmlFor="signup-email"
+                                    weight="medium"
+                                >
+                                    Email
+                                </Text>
+
+                                {emailError && (
+                                    <Text
+                                        className="form_input-message"
+                                        color="red"
+                                    >
+                                        Example error message
+                                    </Text>
+                                )}
+                            </Flex>
+                            <TextField.Root
+                                placeholder="Email"
+                                type="email"
+                                id="signup-email"
+                            />
+                        </Box>
+
+                        <Box className="form_input-container">
+                            <Flex
+                                justify="between"
+                                className="form_label-container"
+                            >
+                                <Text
+                                    as="label"
+                                    htmlFor="signup-password"
+                                    weight="medium"
+                                >
+                                    Password
+                                </Text>
+
+                                {passwordError && (
+                                    <Text
+                                        className="form_input-message"
+                                        color="red"
+                                    >
+                                        Example error message
+                                    </Text>
+                                )}
+                            </Flex>
+                            <TextField.Root
+                                placeholder="Password"
+                                type="password"
+                                id="signup-password"
+                            />
+                        </Box>
+
+                        <Box className="form_button-container">
+                            <Button variant="surface" className="form_button">
+                                Have an account?
+                            </Button>
+
+                            <Button type="submit" className="form_button">
+                                Sign up
+                            </Button>
+                        </Box>
+                    </form>
+                </Card>
+            </Box>
+        </Container>
+    );
 };
 
 export default SignUp;
