5059fda73ea653e78e7340ec73259e8729b33ca6
Jacob <Jacobshortall@gmail.com>
2025-03-31 13:30:34 +0100

    Add error message examples to form

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,32 +1,87 @@
+'use client';
+
 import {
     Box,
     Card,
     Container,
     Heading,
     TextField,
-    Button
+    Button,
+    Text,
+    Flex
 } from '@radix-ui/themes';
+import { useState } from 'react';
 
 const SignIn = () => {
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
     return (
-        <Container>
-            <Box className="form_container" maxWidth="500px">
+        <Container className="standard-wrapper page-wrapper">
+            <Box className="form_container" maxWidth="400px">
                 <Card size="4">
-                    <Heading as="h1" className="form_heading">
+                    <Heading as="h1" className="form_heading" size="7">
                         Sign in
                     </Heading>
 
                     <form>
-                        <TextField.Root
-                            placeholder="Email"
-                            type="email"
-                            id="signin-email"
-                        />
-                        <TextField.Root
-                            placeholder="Password"
-                            type="password"
-                            id="signin-password"
-                        />
+                        <Box className="form_input-container">
+                            <Flex
+                                justify="between"
+                                className="form_label-container"
+                            >
+                                <Text
+                                    as="label"
+                                    htmlFor="signin-email"
+                                    weight="medium"
+                                >
+                                    Email
+                                </Text>
+
+                                {emailError && (
+                                    <Text
+                                        className="form_input-message"
+                                        color="red"
+                                    >
+                                        Example error message
+                                    </Text>
+                                )}
+                            </Flex>
+                            <TextField.Root
+                                placeholder="Email"
+                                type="email"
+                                id="signin-email"
+                            />
+                        </Box>
+
+                        <Box className="form_input-container">
+                            <Flex
+                                justify="between"
+                                className="form_label-container"
+                            >
+                                <Text
+                                    as="label"
+                                    htmlFor="signin-password"
+                                    weight="medium"
+                                >
+                                    Password
+                                </Text>
+
+                                {passwordError && (
+                                    <Text
+                                        className="form_input-message"
+                                        color="red"
+                                    >
+                                        Example error message
+                                    </Text>
+                                )}
+                            </Flex>
+                            <TextField.Root
+                                placeholder="Password"
+                                type="password"
+                                id="signin-password"
+                            />
+                        </Box>
 
                         <Box className="form_button-container">
                             <Button variant="surface" className="form_button">
