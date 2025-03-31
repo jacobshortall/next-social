cf05b945fcd02b4921df1acbc1c7fadc99e211c2
Jacob <Jacobshortall@gmail.com>
2025-03-31 12:14:47 +0100

    Create login form skeleton

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,47 @@
+import {
+    Box,
+    Card,
+    Container,
+    Heading,
+    TextField,
+    Button
+} from '@radix-ui/themes';
+
 const LogIn = () => {
-    return <h1>Log In</h1>;
+    return (
+        <Container>
+            <Box className="form-container" maxWidth="500px">
+                <Card size="4">
+                    <Heading as="h1" className="form-heading">
+                        Log in
+                    </Heading>
+
+                    <form>
+                        <TextField.Root
+                            placeholder="Email"
+                            type="email"
+                            id="login-email"
+                        />
+                        <TextField.Root
+                            placeholder="Password"
+                            type="password"
+                            id="login-password"
+                        />
+
+                        <Box className="form-button-container">
+                            <Button variant="surface" className="form-button">
+                                Create an account
+                            </Button>
+
+                            <Button type="submit" className="form-button">
+                                Log in
+                            </Button>
+                        </Box>
+                    </form>
+                </Card>
+            </Box>
+        </Container>
+    );
 };
 
 export default LogIn;
